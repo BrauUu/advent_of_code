e6ae149ae1c6d78c113d401b9b824ef7fef5de92
BrauUu <brayanfrank.sm@gmail.com>
2025-12-08 09:53:25 -0300

    day 5 part 2

--- 2025/day_5/main.py
+++ 2025/day_5/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
-my_file = ROOT_DIR / "data_test.txt"
+my_file = ROOT_DIR / "data.txt"
 
 data = open(my_file)
 freshCount = 0
@@ -15,8 +15,8 @@ intervals = []
 #         continue
 
 #     if mode == 1:
-#         n1, n2 = map(int, line[:-1].split('-'))
-#         intervals.append([n1,n2])
+#         start, end = map(int, line[:-1].split('-'))
+#         intervals.append([start,end])
 
 #     if mode == 2:
 #         n = int(line[:-1])
@@ -30,44 +30,20 @@ for line in data.readlines():
     if line[:-1] == "":
         break
     if mode == 1:
-        n1, n2 = map(int, line[:-1].split("-"))
-        flag = False
-        for interval in intervals:
-            if (
-                n1 > interval[0]
-                and n1 < interval[1]
-                or n2 > interval[0]
-                and n2 < interval[1]
-            ):
-                interval[1] = n2 if n2 > interval[1] else interval[1]
-                interval[0] = n1 if n1 < interval[0] else interval[0]
-                flag = True
-                break
-        if flag:
-            i = 0
-            while i < len(intervals):
-                currentInterval = intervals[i]
-                n1 = currentInterval[0]
-                n2 = currentInterval[1]
-                for interval in intervals:
-                    if (
-                        n1 > interval[0]
-                        and n1 < interval[1]
-                        or n2 > interval[0]
-                        and n2 < interval[1]
-                    ):
-                        interval[1] = n2 if n2 > interval[1] else interval[1]
-                        interval[0] = n1 if n1 < interval[0] else interval[0]
-                        intervals.pop(i)
-                i += 1
+        start, end = map(int, line[:-1].split("-"))
+        intervals.append([start, end])
 
-        else:
-            intervals.append([n1, n2])
-    
-for interval in intervals:
-    temp = interval[1] - interval[0] + 1
-    freshCount += temp
+intervals.sort()
+intervalsMerged = []
 
+for start, end in intervals:
+    if not intervalsMerged or start > intervalsMerged[-1][1]:
+        intervalsMerged.append([start, end])
+    else:
+        intervalsMerged[-1][1] = max(end, intervalsMerged[-1][1])
+
+for start, end in intervalsMerged:
+    freshCount += (end - start) + 1
 
 print(freshCount)
 data.close()
